5cccc86e63c815518822b74fe8960d72093cc766
chisso2003 <fernandochiossoni@gmail.com>
2024-10-28 14:49:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+$servername = "websales01-server.mysql.database.azure.com";
+$username = "pkoibaloty";
+$password = "mysqlverysecure1!";
+$dbname = "websales01-database";
 
-echo "<h1>Hello, this is my basic PHP website!</h1>";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception   
 
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();   
+
+}
 ?>
